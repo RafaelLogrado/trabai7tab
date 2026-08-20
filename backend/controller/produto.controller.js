59eb5211756e422de93543188878038f1a331bd4
RAFAEL QUEIROZ LOGRADO <rafael_logrado@estudante.sesisenai.org.br>
2026-08-20 10:14:00 -0300

    feito 1 controller

---
+++ backend/controller/produto.controller.js
@@ -0,0 +1,127 @@
+const Produto = require('../models/Produto')
+
+const cadastrar = async (req,res) => {
+    const valores = req.body
+
+    if (!valores.idCategoria || !valores.nome || !valores.preco) {
+        return res.status(400).json({message: "Preencha os campos obrigatórios!"})
+    }
+
+    try{
+        await Produto.create(valores)
+        res.status(201).json({message: `Dados do produtos cadastrados com sucesso!`})
+    }catch(err){
+        res.status(500).json({message: `Não foi possível cadastrar o produto!`})
+        console.error(`Não foi possível cadastrar o produto!`, err)
+    }
+}
+
+const listar = async (req,res) => {
+    try{
+        const dados = await Produto.findAll()
+        res.status(200).json(dados)
+    }catch(err){
+        res.status(500).json({message: `Não foi possível listar os produtos!`})
+        console.error(`Não foi possível listar os produtos!`, err)
+    }
+}
+
+const buscarPorCod = async (req,res) => {
+    const valor = req.params.id
+
+    try{
+        const dado = await Produto.findByPk(valor)
+
+        if(dado){
+            res.status(200).json(dado)
+        }else{
+            res.status(404).json({message: `produto com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível consultar o produto!`})
+        console.error(`Não foi possível consultar o produto!`, err)
+    }
+}
+
+const buscarPorNome = async (req,res) => {
+    const valor = req.params.nome
+
+    try{
+        const dado = await Produto.findOne({where: {nome: valor}})
+
+        if(dado){
+            res.status(200).json(dado)
+        }else{
+            res.status(404).json({message: `produto com nome ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível consultar o produto!`})
+        console.error(`Não foi possível consultar o produto!`, err)
+    }
+}
+
+const atualizarCompleto = async (req,res) => {
+    const valor = req.params.id
+    const valores = req.body
+
+    if (!valores.idCategoria || !valores.nome || !valores.preco) {
+        return res.status(400).json({message: "Preencha os campos obrigatórios!"})
+    }
+
+    try{
+        const dado = await Produto.findByPk(valor)
+
+        if(dado){
+            await dado.update(valores)
+            res.status(200).json({message: `Dados do produto atualizados com sucesso!`})
+        }else{
+            res.status(404).json({message: `produto com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível atualizar o produto!`})
+        console.error(`Não foi possível atualizar o produto!`, err)
+    }
+}
+
+const atualizarParcial = async (req,res) => {
+    const valor = req.params.id
+    const valores = req.body
+
+    if (!valores.nome || !valores.email || !valores.senha || !valores.telefone || !valores.cpf) {
+        return res.status(400).json({message: "Preencha os campos obrigatórios!"})
+    }
+
+    try{
+        const dado = await Produto.findByPk(valor)
+
+        if(dado){
+            await dado.update(valores)
+            res.status(200).json({message: `Dados do produto atualizados com sucesso!`})
+        }else{
+            res.status(404).json({message: `produto com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível atualizar o produto!`})
+        console.error(`Não foi possível atualizar o produto!`, err)
+    }
+}
+
+const excluir = async (req,res) => {
+    const valor = req.params.id
+
+    try{
+        const dado = await Produto.findByPk(valor)
+
+        if(dado){
+            await dado.destroy()
+            res.status(200).json({message: `Dados do produto excluídos com sucesso!`})
+        }else{
+            res.status(404).json({message: `produto com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível excluir o produto!`})
+        console.error(`Não foi possível excluir o produto!`, err)
+    }
+}
+
+module.exports = { cadastrar, listar, buscarPorCod, buscarPorNome, atualizarCompleto, atualizarParcial, excluir }
--- backend/controller/usuario.controller.js
+++ backend/controller/usuario.controller.js
@@ -3,13 +3,125 @@ const Usuario = require('../models/Usuario')
 const cadastrar = async (req,res) => {
     const valores = req.body
 
-    if (!valores.nome || !valores.email || !valores.senha || !valores.telefone || !valores.cpf || !valores.identidade || !valores.tipo_usuario) {
-        return 
+    if (!valores.nome || !valores.email || !valores.senha || !valores.telefone || !valores.cpf) {
+        return res.status(400).json({message: "Preencha os campos obrigatórios!"})
     }
 
     try{
-
+        await Usuario.create(valores)
+        res.status(201).json({message: `Dados do usuários cadastrados com sucesso!`})
     }catch(err){
-        
+        res.status(500).json({message: `Não foi possível cadastrar o usuário!`})
+        console.error(`Não foi possível cadastrar o usuário!`, err)
     }
 }
+
+const listar = async (req,res) => {
+    try{
+        const dados = await Usuario.findAll()
+        res.status(200).json(dados)
+    }catch(err){
+        res.status(500).json({message: `Não foi possível listar os usuários!`})
+        console.error(`Não foi possível listar os usuários!`, err)
+    }
+}
+
+const buscarPorCod = async (req,res) => {
+    const valor = req.params.id
+
+    try{
+        const dado = await Usuario.findByPk(valor)
+
+        if(dado){
+            res.status(200).json(dado)
+        }else{
+            res.status(404).json({message: `Usuário com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível consultar o usuário!`})
+        console.error(`Não foi possível consultar o usuário!`, err)
+    }
+}
+
+const buscarPorNome = async (req,res) => {
+    const valor = req.params.nome
+
+    try{
+        const dado = await Usuario.findOne({where: {nome: valor}})
+
+        if(dado){
+            res.status(200).json(dado)
+        }else{
+            res.status(404).json({message: `Usuário com nome ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível consultar o usuário!`})
+        console.error(`Não foi possível consultar o usuário!`, err)
+    }
+}
+
+const atualizarCompleto = async (req,res) => {
+    const valor = req.params.id
+    const valores = req.body
+
+    if (!valores.nome || !valores.email || !valores.senha || !valores.telefone || !valores.cpf) {
+        return res.status(400).json({message: "Preencha os campos obrigatórios!"})
+    }
+
+    try{
+        const dado = await Usuario.findByPk(valor)
+
+        if(dado){
+            await dado.update(valores)
+            res.status(200).json({message: `Dados do usuário atualizados com sucesso!`})
+        }else{
+            res.status(404).json({message: `Usuário com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível atualizar o usuário!`})
+        console.error(`Não foi possível atualizar o usuário!`, err)
+    }
+}
+
+const atualizarParcial = async (req,res) => {
+    const valor = req.params.id
+    const valores = req.body
+
+    if (!valores.nome || !valores.email || !valores.senha || !valores.telefone || !valores.cpf) {
+        return res.status(400).json({message: "Preencha os campos obrigatórios!"})
+    }
+
+    try{
+        const dado = await Usuario.findByPk(valor)
+
+        if(dado){
+            await dado.update(valores)
+            res.status(200).json({message: `Dados do usuário atualizados com sucesso!`})
+        }else{
+            res.status(404).json({message: `Usuário com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível atualizar o usuário!`})
+        console.error(`Não foi possível atualizar o usuário!`, err)
+    }
+}
+
+const excluir = async (req,res) => {
+    const valor = req.params.id
+
+    try{
+        const dado = await Usuario.findByPk(valor)
+
+        if(dado){
+            await dado.destroy()
+            res.status(200).json({message: `Dados do usuário excluídos com sucesso!`})
+        }else{
+            res.status(404).json({message: `Usuário com código ${valor} não encontrado!`})
+        }
+    }catch(err){
+        res.status(500).json({message: `Não foi possível excluir o usuário!`})
+        console.error(`Não foi possível excluir o usuário!`, err)
+    }
+}
+
+module.exports = { cadastrar, listar, buscarPorCod, buscarPorNome, atualizarCompleto, atualizarParcial, excluir }
